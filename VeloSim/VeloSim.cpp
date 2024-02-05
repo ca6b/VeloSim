a4bba0ca5d82dbf3dfabf9db4ee117dd31c7a5e6
ca6b <58319429+ca6b@users.noreply.github.com>
2024-02-04 23:13:20 -0500

    project setup

--- VeloSim/VeloSim.cpp
+++ VeloSim/VeloSim.cpp
@@ -1,12 +1,7 @@
 ﻿// VeloSim.cpp : Defines the entry point for the application.
 //
 
-#include "VeloSim.h"
-
-using namespace std;
-
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+   
 }
